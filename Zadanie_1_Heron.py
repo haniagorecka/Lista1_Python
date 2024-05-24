c54597c578584f43489084be44cc7b9f1179dd7f
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-05-24 23:14:26 +0200

    Zadanie 1 - komentarz

--- Zadanie_1_Heron.py
+++ Zadanie_1_Heron.py
@@ -1,5 +1,15 @@
 
 def heron (s1, s2, s3):
+    """
+    Funkcja zwraca powierzchnię trójkąta z boków podanych jako argumenty
+    :param s1: pierwszy bok trójkąta, dodatni
+    :param s2: drugi bok trójkąta, dodatni
+    :param s3: trzeci bok trójkąta, dodatni
+    :return: powierzchnię trójkąta
+    :raise: ValueError jeśli któryś z boków jest niedodatni
+    :raise: ValueError jeśli jedna z par boków nie jest większa niż ostatni bok
+    :author: Hanna Górecka
+    """
     if (s1 <= 0 or s2 <= 0 or s3 <= 0):
         raise  ValueError("Boki muszą być dodatnie")
     if (s1 + s2 <= s3 or s1 + s3 <= s2 or s2 + s3 <= s1):
