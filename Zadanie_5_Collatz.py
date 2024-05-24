66aa5e49756a0303b2fc77f5dfa572262f6cc698
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-05-24 22:59:49 +0200

    Zadanie 5 - komentarz

--- Zadanie_5_Collatz.py
+++ Zadanie_5_Collatz.py
@@ -1,4 +1,12 @@
 def collatz(c0):
+    """
+    Funkcja zwraca listę elementów ciągu collatza zaczynającego się od c0 przed tym jak wpadnie w cykl, przyjmuję, że cykl zaczyna się
+    od pierwszego elementu równego 4 włącznie, więc nie wypisuję już tego elementu
+    :author: Hanna Górecka
+    :param c0:pierwszy element ciągu
+    :return:lista elementów ciągu przed wpadnięciem w cykl
+    :raise: ValueError jeśli pierwszy element ciągu jest niedodatni
+    """
     if c0<=0:
         raise ValueError("Bledny pierwszy element ciagu")
     c1 = c0
